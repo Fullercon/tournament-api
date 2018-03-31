2c89aaa2bbb77e6bbfd4514f88b042aa69afcf52
wolfe111 <jackscwolfe@hotmail.com>
2018-03-31 17:41:22 +0100

    [TAPI-04] - Fixed integration test :)

--- src/test/java/com/reemteam/tournamentapi/integrationTests/TournamentIT.groovy
+++ src/test/java/com/reemteam/tournamentapi/integrationTests/TournamentIT.groovy
@@ -2,34 +2,23 @@ package com.reemteam.tournamentapi.integrationTests
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.reemteam.tournamentapi.TournamentApiApplication
-import com.reemteam.tournamentapi.controller.TournamentController
-import com.reemteam.tournamentapi.model.Tournament
 import com.reemteam.tournamentapi.service.TournamentRepository
-import jdk.internal.org.objectweb.asm.TypeReference
-import org.apache.tools.ant.util.TeeOutputStream
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.core.io.ClassPathResource
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 
-import static org.mockito.Mockito.*
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TournamentApiApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class TournamentIT {
@@ -37,25 +26,16 @@ class TournamentIT {
     @Autowired
     private MockMvc mockMvc
 
-    @MockBean
+    @Autowired
     private TournamentRepository tournamentRepository
-
-    @Before
-    void setup () {
-
-    }
 
     @Test
     void shouldReturnAllTournaments() {
         ObjectMapper objectMapper = new ObjectMapper()
-        File file = new ClassPathResource("allTournaments.json").getFile();
-        List<Tournament> expected = objectMapper.readValue(file, Tournament[].class)
+        File file = new ClassPathResource("allTournaments.json").getFile()
 
         mockMvc.perform(get("/api/tournaments"))
         .andExpect(status().is(200))
         .andExpect(content().json(file.getText()))
-
     }
-
-
 }
